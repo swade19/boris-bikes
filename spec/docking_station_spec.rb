315baad1366f695e29b24899041983adaed8e07c
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-11 14:58:07 +0100

    Created test for dockingStation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,2 +1,9 @@
+require './lib/docking_station.rb'
+
 describe DockingStation do
+    it 'should create a new instance of the DockingStation class' do
+        new_dock = DockingStation.new
+
+        expect(new_dock).to be_a(DockingStation)
+    end
 end
