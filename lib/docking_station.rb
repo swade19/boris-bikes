8b31f0926403fc609b076db6223c39b7b223d785
krisn64209 <krisnorris@gmail.com>
2019-10-11 15:32:52 +0100

    added tests for creating bikes and docks

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,12 @@
 class DockingStation
-    
+  
+  def release_bike
+    new_bike = Bike.new
+  end
+end
+
+class Bike
+  def working
+    return 'Bike working'
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,27 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
     it 'should create a new instance of the DockingStation class' do
-        new_dock = DockingStation.new
-
-        expect(new_dock).to be_a(DockingStation)
+      new_dock = DockingStation.new
+      expect(new_dock).to be_a(DockingStation)
     end
+
+    it 'should release bike from docking station' do
+      new_dock = DockingStation.new
+      new_dock.release_bike
+      expect(new_dock.release_bike).to be_a(Bike)
+    end
+  
+end
+
+describe Bike do
+  it 'creates new instance of bike class' do
+    new_bike = Bike.new
+    expect(new_bike).to be_a(Bike)
+  end
+  
+  it 'tells you if the bike is working' do
+    new_bike = Bike.new
+    new_bike.working
+    expect(new_bike.working).to eq 'Bike working'
+  end
 end
