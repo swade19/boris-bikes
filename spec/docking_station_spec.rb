39f8069e8ce0bc8d73712a07e395690b8e99da2c
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-15 15:41:43 +0100

    added failing test for not working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -69,6 +69,9 @@ describe DockingStation do
     end
 
     it "allows people to report broken bikes during docking" do
-      
+      new_dock = DockingStation.new
+      new_bike = Bike.new
+      new_bike.not_working
+      expect(new_bike.not_working).to eq(not_working != working?)
     end
 end
